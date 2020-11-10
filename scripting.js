c1fa7dd68b13532516a41b5b76bf54e026136304
Mattheaus <69998724+Mattheaus@users.noreply.github.com>
2020-11-10 03:17:20 -0600

    Update scripting.js

--- scripting.js
+++ scripting.js
@@ -1,30 +1,3 @@
-
-
-function piecesize() {
-  let craneimg = document.querySelector("#crane");
-  let pieceimg = document.querySelector("#piece");
-  let wratio = pieceimg.naturalWidth/craneimg.naturalWidth;
-  let hratio = pieceimg.naturalHeight/craneimg.naturalHeight;
-  pieceimg.style.height = craneimg.height*hratio + "px";
-  pieceimg.style.width = craneimg.width*wratio + "px";
-}
-function placement() {
-  let craneimg = document.querySelector("#crane");
-  let pieceimg = document.querySelector("#piece");
-  pieceimg.style.bottom = craneimg.height*0.453 + "px";
-  pieceimg.style.left = craneimg.width*0.758 + "px";
-}
-function movingcrane(){
-  let craneimg = document.querySelector("#crane");
-  let scrollPercent = scroll/window.height;
-  craneimg.style.left = window.width*scrollPercent + 'px';
-}
-function main(){
-  piecesize();
-  placement();
-}
-
-
 window.onresize = main;
 window.onload = main;
 window.addEventListener('scroll', function(event) {
