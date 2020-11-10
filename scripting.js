e2819e743f2268aa3d0a988593a3c8b0f847f842
Mattheaus <69998724+Mattheaus@users.noreply.github.com>
2020-11-10 03:25:42 -0600

    Update scripting.js

--- scripting.js
+++ scripting.js
@@ -3,6 +3,6 @@ window.addEventListener('scroll', function(event) {
   let hlimit = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
   let wlimit = Math.max( document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth );
   console.log(scrollY);
-  let scrollPercent = scrollY/(hlimit*1.2);
+  let scrollPercent = scrollY/(hlimit*1.4);
   craneimg.style.left = wlimit*scrollPercent + 'px';
 });
