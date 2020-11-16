53465cc28b75048d1f3869386c246fc32e97838b
Mattheaus <69998724+Mattheaus@users.noreply.github.com>
2020-11-16 01:58:30 -0600

    fixing issues

--- scripting.js
+++ scripting.js
@@ -1,7 +1,23 @@
 window.addEventListener('scroll', function(event) {
-  let craneimg = document.querySelector("#crane");
-  let hlimit = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
-  let wlimit = Math.max( document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth );
-  let scrollPercent = scrollY/(hlimit*1.4);
-  craneimg.style.left = wlimit*scrollPercent + 'px';
+    let craneimg = document.querySelector("#crane");
+    let hlimit = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+    let wlimit = Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);
+    let scrollPercent = scrollY / (hlimit * 1.4);
+    craneimg.style.left = wlimit * scrollPercent + 'px';
 });
+
+window.onload = function() {
+    let title = document.getElementById('title');
+    let background = document.getElementById("all");
+    let section1 = document.getElementById("sec1");
+    let section2 = document.getElementById("sec2");
+    let section3 = document.getElementById("sec3");
+    let section4 = document.getElementById("sec4");
+    let section5 = document.getElementById("sec5");
+    title.style.left = background.offsetWidth / 2 - title.offsetWidth / 2 + "px";
+    section1.style.top = title.offsetTop + title.offsetHeight + window.innerHeight * 0.01 + "px";
+    section2.style.top = section1.offsetTop + section1.offsetHeight + window.innerHeight * 0.005 + "px";
+    section3.style.top = title.offsetTop + title.offsetHeight + window.innerHeight * 0.01 + "px";
+    section4.style.top = section3.offsetTop + section3.offsetHeight + window.innerHeight * 0.005 + "px";
+    section5.style.top = title.offsetTop + title.offsetHeight + window.innerHeight * 0.01 + "px";
+};
